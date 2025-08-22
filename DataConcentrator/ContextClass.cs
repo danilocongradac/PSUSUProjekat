f47cf2dec1442c2e671cba939560fd81124ed202
Danilo Congradac <congradacd002@gmail.com>
2025-08-22 19:51:37 +0200

    Dodavanje tagova i vrednosti u WPF
    WPF, dodavanje tagova, promena vrednosti tagova, provere...
    
    Sledece:
    uklanjanje tagova
    dodavanje alarma
    brisanje alarma
    lepsi prikaz tagova
    
    prikaz aktivnih alarma

--- DataConcentrator/ContextClass.cs
+++ DataConcentrator/ContextClass.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataConcentrator
 {
-    internal class ContextClass
+    public class ContextClass : DbContext
     {
+        public ContextClass() : base("name=SCADA_DB")
+        {
+            Database.SetInitializer(
+                new DropCreateDatabaseIfModelChanges<ContextClass>());
+        }
+
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Alarm> Alarms { get; set; }
+        public DbSet<ActivatedAlarm> ActivatedAlarms { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Relacija Tag → Alarms
+            modelBuilder.Entity<Tag>()
+                .HasMany(t => t.Alarms)
+                .WithOptional()
+                .WillCascadeOnDelete(true);
+
+           
+            base.OnModelCreating(modelBuilder);
+        }
     }
+
 }
